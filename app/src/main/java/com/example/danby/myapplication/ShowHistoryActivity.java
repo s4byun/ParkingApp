65d2ce4642109877d43b1f1862f35a8bba14cf90
DavidWoo <david8147@gmail.com>
2017-06-19 09:07:11 -0700

    new commit

---
+++ app/src/main/java/com/example/danby/myapplication/ShowHistoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.danby.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class ShowHistoryActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_history);
+    }
+}
